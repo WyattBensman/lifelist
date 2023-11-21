8ab7c8f7bd539b62aa6edb4aa4ae5bc3080a06d0
WyattBensman <wyattbensman5@gmail.com>
2023-11-21 08:39:06 -0500

    Created connection & server, deleted Authentication / not needed

---
+++ server/config/connection.js
@@ -0,0 +1,9 @@
+const { connect, connection } = require("mongoose");
+
+// DB Name goes at the end ------->
+connect(
+  process.env.MONGODB_URI ||
+    "mongodb://127.0.0.1:27017/lifelistEarlyAccessSignups"
+);
+
+module.exports = connection;
