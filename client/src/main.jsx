70e800d423aec5fc484557710720392d9ac174c9
WyattBensman <wyattbensman5@gmail.com>
2023-11-28 13:32:45 -0500

    help

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
