0b98c24dd33149219b3e79e61dd4685787e413f2
WyattBensman <wyattbensman5@gmail.com>
2023-11-28 12:19:54 -0500

    Fix Vite configuration for Heroku

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+const react = require("@vitejs/plugin-react-swc");
 
 // https://vitejs.dev/config/
 export default defineConfig({
